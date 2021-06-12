543e260db116c8136dd57a8c588bb2c09d7bb19e
Pupilsch <821058047@qq.com>
2021-06-12 01:34:43 -0700

    First commit

---
+++ Server/Server.cpp
@@ -0,0 +1,40 @@
+#include "Server.h"
+
+static bool stop = false;
+
+static void handle_term(int sig){
+    stop = true;
+}
+
+int main(int argc, char *argv[]){
+    signal(SIGTERM, handle_term); // Receive signal from outside to end this program
+
+    if (argc <= 3){
+        std::cout << "usage: " << basename(argv[0]) << " ip_address port_number backlog\n" << std::endl;
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+    int backlog = atoi(argv[3]);
+
+    int sock = socket( PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+
+    int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(sock, backlog);
+    assert(ret != -1);
+
+    while(!stop){
+        sleep(1);
+    }
+
+    close(sock);
+    return 0;
+
+}
---
+++ Server/Server.h
@@ -0,0 +1,16 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#include <stdio.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+#endif
